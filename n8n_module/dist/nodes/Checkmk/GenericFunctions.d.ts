7417a84e0866e007372fa658694dbb10c83d0968
Marco Gonçalves <marco.goncalves2@sky.uk>
2025-10-14 16:36:15 +0100

    chore: moves folder content to root adds dist to gitignore

--- n8n_module/dist/nodes/Checkmk/GenericFunctions.d.ts
+++
@@ -1,3 +0,0 @@
-import { IExecuteFunctions, IHookFunctions, ILoadOptionsFunctions, IHttpRequestMethods } from 'n8n-workflow';
-export declare function checkmkApiRequest(this: IExecuteFunctions | ILoadOptionsFunctions | IHookFunctions, method: IHttpRequestMethods, endpoint: string, body?: any, qs?: any): Promise<any>;
-export declare function checkmkApiRequestAllItems(this: IExecuteFunctions | ILoadOptionsFunctions | IHookFunctions, method: IHttpRequestMethods, endpoint: string, body?: any, qs?: any): Promise<any>;

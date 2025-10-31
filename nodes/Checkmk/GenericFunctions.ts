bf1448ab5abab58d8979464fa7673f6d7a35ac4a
EllieLynx <elicarlos.dias@lynxmind.com>
2025-10-31 09:01:37 +0000

    Update GenericFunctions.ts

--- nodes/Checkmk/GenericFunctions.ts
+++ nodes/Checkmk/GenericFunctions.ts
@@ -3,9 +3,9 @@ import {
 	IHookFunctions,
 	ILoadOptionsFunctions,
 	IHttpRequestMethods,
+	IRequestOptions,
 	JsonObject,
 	NodeApiError,
-	IHttpRequestOptions,
 } from 'n8n-workflow';
 
 export async function checkmkApiRequest(
@@ -14,10 +14,11 @@ export async function checkmkApiRequest(
 	endpoint: string,
 	body: any = {},
 	qs: any = {},
+	customHeaders: any = {},
 ): Promise<any> {
 	const credentials = await this.getCredentials('checkmkApi');
 
-	const options: IHttpRequestOptions = {
+	const options: IRequestOptions = {
 		method,
 		body,
 		qs,
@@ -26,12 +27,13 @@ export async function checkmkApiRequest(
 			'Content-Type': 'application/json',
 			Accept: 'application/json',
 			Authorization: `Bearer ${credentials.username} ${credentials.password}`,
+			...customHeaders,
 		},
 		json: true,
 	};
 
 	try {
-		return await this.helpers.httpRequest(options);
+		return await this.helpers.request(options);
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error as JsonObject);
 	}
@@ -49,7 +51,7 @@ export async function checkmkApiRequestAllItems(
 
 	do {
 		responseData = await checkmkApiRequest.call(this, method, endpoint, body, qs);
-
+		
 		if (responseData.value) {
 			returnData.push(...responseData.value);
 		} else if (Array.isArray(responseData)) {
@@ -68,3 +70,39 @@ export async function checkmkApiRequestAllItems(
 
 	return returnData;
 }
+
+export async function checkmkApiRequestWithETag(
+	this: IExecuteFunctions | ILoadOptionsFunctions | IHookFunctions,
+	method: IHttpRequestMethods,
+	endpoint: string,
+	body: any = {},
+	qs: any = {},
+): Promise<{ data: any; etag: string }> {
+	const credentials = await this.getCredentials('checkmkApi');
+
+	const options: IRequestOptions = {
+		method,
+		body,
+		qs,
+		url: `${credentials.host}/${credentials.site}/check_mk/api/1.0${endpoint}`,
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+			Authorization: `Bearer ${credentials.username} ${credentials.password}`,
+		},
+		json: true,
+		resolveWithFullResponse: true,
+	};
+
+	try {
+		const response = await this.helpers.request(options);
+		const etag = response.headers.etag || response.headers['etag'];
+		return {
+			data: response.body,
+			etag: etag ? etag.replace(/"/g, '') : '', // Remove quotes from ETag
+		};
+	} catch (error) {
+		throw new NodeApiError(this.getNode(), error as JsonObject);
+	}
+}
+
